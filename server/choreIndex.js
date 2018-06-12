25abb751cfaf1b0fa88b6f31de10af2b5fce211e
Chris <chriswfoster@gmail.com>
2018-06-12 13:25:17 -0500

    building index

--- server/choreIndex.js
+++ server/choreIndex.js
@@ -1,14 +1,22 @@
-const express = require('express');
-const cors = require('cors')
-const {json} = require('body-parser')
+const express = require("express")
+const cors = require("cors")
+const { json } = require("body-parser")
+require("dotenv").config()
 
-const PORT = 3055;
+const PORT = 3055
 
 const app = express()
+app.use(express.static(`${__dirname}/../build`))
 app.use(cors())
 app.use(json())
 
+const massiveConnection = massive(process.env.connectionString)
+  .then(db => app.set("db", db))
+  .catch(console.log)
 
-
+const path = require("path")
+app.get("*", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "/../build/index.html"))
+})
 
 app.listen(PORT, console.log(`Now turned into ${PORT} FM`))
