12cc4db7df787324d676b192b407c961097c5f34
Chris <chriswfoster@gmail.com>
2018-06-12 13:36:54 -0500

    added axios and fixed index bug

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,11 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import router from '../router'
-import './App.css';
+import React, { Component } from "react"
+import router from "../router"
+import "./App.css"
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        {router}
-      </div>
-    );
+    return <div className="App">{router}</div>
   }
 }
 
-export default App;
+export default App
