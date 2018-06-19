194ba45aeaa9db6ba3a0bdccc3262a28c89d1fb8
Chris <chriswfoster@gmail.com>
2018-06-19 18:35:04 -0500

    Added sockets to index code

--- server/choreIndex.js
+++ server/choreIndex.js
@@ -1,37 +1,34 @@
 require("dotenv").config()
+const http = require("http")
 const cors = require("cors")
 const express = require("express")
-const massive = require('massive')
+const massive = require("massive")
 const { json } = require("body-parser")
+const socketIo = require("socket.io")
 const ctrl = require("./controllers/mainController")
 
 const PORT = 3055
+
 
 const app = express()
 app.use(express.static(`${__dirname}/../build`))
 app.use(cors())
 app.use(json())
 
+const server = http.createServer(app)
+const io = socketIo(server)
+
 const massiveConnection = massive(process.env.connectionString)
   .then(db => app.set("db", db))
   .catch(console.log)
 
-
-
-app.get('/api/getChildren', ctrl.getChildren)
-app.get('/api/getChores', ctrl.getChores)
-app.post('/api/addChore', ctrl.addChore)
-
-
-
-
-
-
-
+app.get("/api/getChildren", ctrl.getChildren)
+app.get("/api/getChores", ctrl.getChores)
+app.post("/api/addChore", ctrl.addChore)
 
 const path = require("path")
 app.get("*", (req, res, next) => {
   res.sendFile(path.join(__dirname, "/../build/index.html"))
 })
 
-app.listen(PORT, console.log(`Now tuned into ${PORT} FM`))
+server.listen(PORT, console.log(`Now tuned into ${PORT} FM`))
